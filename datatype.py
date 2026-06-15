ecba23174011ef18df352e437512adc8c8872d42
irinrajani6-web <irinrajani6@gmail.com>
2026-06-15 07:54:14 +0000

    Added datatype.py

---
+++ datatype.py
@@ -0,0 +1,29 @@
+a = 10
+print(a, type(a))
+
+b = 10.5
+print(b, type(b))
+
+c = "Hello"
+print(c, type(c))
+
+d = True
+print(d, type(d))
+
+e = [1, 2, 3]
+print(e, type(e))
+
+f = (1, 2, 3)
+print(f, type(f))
+
+g = {1, 2, 3}
+print(g, type(g))
+
+h = {"name": "Irin", "age": 17}
+print(h, type(h))
+
+i = 2 + 3j
+print(i, type(i))
+
+j = None
+print(j, type(j))
